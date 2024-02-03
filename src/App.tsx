446647053c03f7d036ebb2a2f96b1290ad5e44c7
Boyan Kaloyanov <bnkaloyanov@gmail.com>
2024-02-03 21:48:15 +0200

    moved ship origin to center & added rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { createEffect, createSignal, onMount } from 'solid-js';
+import { createEffect, createSignal } from 'solid-js';
 import './App.css';
 import { ShipPosition } from './models/ship-position.model';
-import { Position } from './models/position.model';
-import { Matrix3x3 } from './models/matrix3x3.model';
 import { Ship } from './models/ship.model';
+import { getAngleBetweenPoints, getDistanceBetweenPoints } from './utils/points';
 
 const CANVAS_LENGTH = 1600;
 const CANVAS_WIDTH = 800;
@@ -15,11 +14,15 @@ const IDENTITY_SHIP_SIDE_OFFSET = 8;
 const IDENTITY_SHIP_LENGTH_OFFSET = IDENTITY_SHIP_LENGTH - IDENTITY_SHIP_SIDE_OFFSET;
 const IDENTITY_SHIP_WIDTH_OFFSET = IDENTITY_SHIP_WIDTH - IDENTITY_SHIP_SIDE_OFFSET;
 
+const ROTATE_TOOL_WIDTH = 10;
+
 function App() {
   const [context, setContext] = createSignal<CanvasRenderingContext2D>();
   const [color, setColor] = createSignal<string>('blue');
   const [scale, setScale] = createSignal<number>(1);
+  const [mouseHoldStart, setMouseHoldStart] = createSignal<DOMPointReadOnly | null>(null);
   const [selectedShipId, setSelectedShipId] = createSignal<string | null>(null);
+  const [activeCommand, setActiveCommand] = createSignal<'pan' | 'rotate' | null>(null);
   // const [scale, setScale] = createSignal<Matrix3x3>(Matrix3x3.eye);
 
   // const [ships, setShips] = createSignal<Ship[]>([
@@ -42,12 +45,13 @@ function App() {
   // ]);
 
   const [ships, setShips] = createSignal<Ship[]>([
-    { id: '1', position: { origin: new DOMPoint(0, 0), rotation: 0 } },
-    { id: '2', position: { origin: new DOMPoint(50, 50), rotation: 30 } },
-    { id: '3', position: { origin: new DOMPoint(100, 100), rotation: 45 } },
-    { id: '4', position: { origin: new DOMPoint(150, 150), rotation: 60 } },
-    { id: '5', position: { origin: new DOMPoint(200, 200), rotation: 75 } },
-    { id: '6', position: { origin: new DOMPoint(250, 250), rotation: 90 } },
+    { id: '1', position: { origin: new DOMPointReadOnly(0, 0), rotation: 0 } },
+    { id: '2', position: { origin: new DOMPointReadOnly(50, 50), rotation: 30 } },
+    { id: '3', position: { origin: new DOMPointReadOnly(100, 100), rotation: 45 } },
+    { id: '4', position: { origin: new DOMPointReadOnly(150, 150), rotation: 60 } },
+    { id: '5', position: { origin: new DOMPointReadOnly(200, 200), rotation: 75 } },
+    { id: '6', position: { origin: new DOMPointReadOnly(250, 250), rotation: 90 } },
+    { id: '7', position: { origin: new DOMPointReadOnly(300, 300), rotation: 180 } },
 
     // new DOMMatrix('translate(0px, 0px) rotateZ(0) scale(1, 1)'),
     // new DOMMatrix('translate(50px, 50px) rotateZ(30deg) scale(1, 1)'),
@@ -60,13 +64,16 @@ function App() {
   const createShip = (): Path2D => { 
     const shipPath = new Path2D();
 
-    shipPath.moveTo(0, 0);
-    shipPath.lineTo(IDENTITY_SHIP_WIDTH, 0);
-    shipPath.lineTo(IDENTITY_SHIP_WIDTH, IDENTITY_SHIP_LENGTH_OFFSET);
-    shipPath.lineTo(IDENTITY_SHIP_WIDTH_OFFSET, IDENTITY_SHIP_LENGTH);
-    shipPath.lineTo(IDENTITY_SHIP_SIDE_OFFSET, IDENTITY_SHIP_LENGTH);
-    shipPath.lineTo(0, IDENTITY_SHIP_LENGTH_OFFSET);
-    shipPath.lineTo(0, 0);
+    const halfWidth = IDENTITY_SHIP_WIDTH / 2;
+    const halfLength = IDENTITY_SHIP_LENGTH / 2;
+
+    shipPath.moveTo((-halfWidth) + 0, (-halfLength) + 0);
+    shipPath.lineTo((-halfWidth) + IDENTITY_SHIP_WIDTH, (-halfLength) + 0);
+    shipPath.lineTo((-halfWidth) + IDENTITY_SHIP_WIDTH, (-halfLength) + IDENTITY_SHIP_LENGTH_OFFSET);
+    shipPath.lineTo((-halfWidth) + IDENTITY_SHIP_WIDTH_OFFSET, (-halfLength) + IDENTITY_SHIP_LENGTH);
+    shipPath.lineTo((-halfWidth) + IDENTITY_SHIP_SIDE_OFFSET, (-halfLength) + IDENTITY_SHIP_LENGTH);
+    shipPath.lineTo((-halfWidth) + 0, (-halfLength) + IDENTITY_SHIP_LENGTH_OFFSET);
+    shipPath.lineTo((-halfWidth) + 0, (-halfLength) + 0);
 
     return shipPath;
   }
@@ -83,32 +90,21 @@ function App() {
   // }
 
   const drawShip = (ship: Ship) => {
-    const ctx = context()!;
-
-    ctx.strokeStyle = color();
-    // ctx.resetTransform();
-    ctx.setTransform(getShipDomMatrix(ship.position));
     
     if (selectedShipId() === ship.id) {
-      ctx.beginPath();
-      console.log('arc center', ship.position.origin.x + (IDENTITY_SHIP_WIDTH / 2), ship.position.origin.y + (IDENTITY_SHIP_LENGTH / 2))
-
-      ctx.arc((IDENTITY_SHIP_WIDTH / 2), (IDENTITY_SHIP_LENGTH / 2), (IDENTITY_SHIP_LENGTH * scale()) / 2 + 10, 0, 2 * Math.PI);
-      ctx.stroke();
-
-      ctx.beginPath();
-      ctx.arc((IDENTITY_SHIP_WIDTH / 2), (IDENTITY_SHIP_LENGTH / 2), 1, 0, 2*Math.PI)
-      
-      ctx.stroke();
+      drawShipTools(ship);
     }
-
+    
     // ctx.resetTransform();
-
+    
     // const shipPosition = ship.position;
     // const path = new Path2D();
     // const shipTransformationMatrix = getShipDomMatrix(shipPosition);
     // path.addPath(createShip(), shipTransformationMatrix)
     
+    const ctx = context()!;
+    ctx.strokeStyle = color();
+    ctx.setTransform(getShipDomMatrix(ship.position));
     ctx.stroke(createShip());
     ctx.beginPath();
     // ctx.stroke(path);
@@ -155,63 +151,255 @@ function App() {
     context.resetTransform();
   });
 
+  const drawShipTools = (ship: Ship): void => {
+
+    const ctx = context()!;
+
+    ctx.strokeStyle = color();
+    ctx.setTransform(getShipDomMatrix(ship.position));
+    
+    ctx.beginPath();
+    ctx.arc(0, 0, panToolRadius(), 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(0, 0, rotateToolInnerRadius(), 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(0, 0, rotateToolOuterRadius(), 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(0, 0, 1, 0, 2*Math.PI)
+    
+    ctx.stroke();
+  }
+
   const getShipDomMatrix = (shipPosition: ShipPosition): DOMMatrix => {
     return new DOMMatrix(
       `translate(${shipPosition.origin.x}px, ${shipPosition.origin.y}px) rotateZ(${shipPosition.rotation}deg) scale(${scale()}, ${scale()})`
     );
   }
 
-  const isPositionInShip = (position: Position, shipMatrix: DOMMatrix): boolean => {
-    const inverseMatrix = shipMatrix.inverse();
-
-    const domPoint = inverseMatrix.transformPoint(new DOMPoint(position.x, position.y));
-    
-    return isPointInIdentityShip(domPoint);
+  const getSelectedShip = (): Ship | null => {
+    return ships().find(s => s.id === selectedShipId()) || null;
   }
 
-  const findClickedShipId = (position: Position): string | null => {
+  const panToolRadius = () => {
+    return scaledShipLength() * 0.2;
+  }
+
+  const rotateToolInnerRadius = () => {
+    return scaledShipLength() * 0.6;
+  } 
+
+  const rotateToolOuterRadius = () => {
+    return rotateToolInnerRadius() + (ROTATE_TOOL_WIDTH * scale());
+  }
+
+  const scaledShipLength = (): number => {
+    return IDENTITY_SHIP_LENGTH * scale();
+  }
+
+  const scaledShipWidth = (): number => {
+    return IDENTITY_SHIP_WIDTH * scale();
+  }
+
+  const isPositionInShip = (position: DOMPointReadOnly, shipMatrix: DOMMatrix): boolean => {
+    const inverseMatrix = shipMatrix.inverse();
+
+    const DOMPointReadOnly = inverseMatrix.transformPoint(position);
     
-    const currentShips = ships();
+    return isPointInIdentityShip(DOMPointReadOnly);
+  }
+
+  const findClickedShip = (position: DOMPointReadOnly): Ship | null => {
+    
     for (let index = 0; index < ships().length; index++) {
-      const ship = currentShips[index];
+      const ship = ships()[index];
       
       if (isPositionInShip(position, getShipDomMatrix(ship.position))) {
-        return ship.id;
+        return ship;
       }
     }
 
     return null;
   }
 
-  const isPointInIdentityShip = (point: DOMPoint): boolean => {
-    const isWithinXBounds = point.x >= 0 && point.x <= IDENTITY_SHIP_WIDTH;
-    const isWithinYBounds = point.y >= 0 && point.y <= IDENTITY_SHIP_LENGTH;
+  const isPointInIdentityShip = (point: DOMPointReadOnly): boolean => {
+    // point is adjusted to compensate for the fact that the ship origin is in the center of the ship, instead of an endpoint
+    const adjustedPoint = new DOMPointReadOnly(point.x + IDENTITY_SHIP_WIDTH / 2, point.y + IDENTITY_SHIP_LENGTH / 2);
+
+    const isWithinXBounds = adjustedPoint.x >= 0 && adjustedPoint.x <= IDENTITY_SHIP_WIDTH;
+    const isWithinYBounds = adjustedPoint.y >= 0 && adjustedPoint.y <= IDENTITY_SHIP_LENGTH;
 
     if (isWithinXBounds && isWithinYBounds) {
-      if (point.y <= IDENTITY_SHIP_LENGTH_OFFSET) {
+      if (adjustedPoint.y <= IDENTITY_SHIP_LENGTH_OFFSET) {
         return true;
       }
   
-      if (point.y <= IDENTITY_SHIP_LENGTH) {
-        return (point.x <= IDENTITY_SHIP_WIDTH_OFFSET && point.x >= (point.y - IDENTITY_SHIP_LENGTH_OFFSET)) 
-            || (point.x > IDENTITY_SHIP_WIDTH_OFFSET && point.x + (point.y - IDENTITY_SHIP_LENGTH_OFFSET) <= IDENTITY_SHIP_WIDTH);
+      if (adjustedPoint.y <= IDENTITY_SHIP_LENGTH) {
+        return (adjustedPoint.x <= IDENTITY_SHIP_WIDTH_OFFSET && adjustedPoint.x >= (adjustedPoint.y - IDENTITY_SHIP_LENGTH_OFFSET)) 
+            || (adjustedPoint.x > IDENTITY_SHIP_WIDTH_OFFSET && adjustedPoint.x + (adjustedPoint.y - IDENTITY_SHIP_LENGTH_OFFSET) <= IDENTITY_SHIP_WIDTH);
       }
     }
 
     return false;
   }
 
-  const handleMouseDown = (event: MouseEvent) => {
-    
-    const canvas = document.getElementById('canvas')!;
+  const isClickInTools = (ship: Ship, click: DOMPointReadOnly) => {
 
+    const shipCenter = ship.position.origin;
+    return getDistanceBetweenPoints(shipCenter, click) <= rotateToolOuterRadius();
+  }
+
+  const isClickOnPan = (click: DOMPointReadOnly): boolean => {
+    const selectedShip = getSelectedShip()!;
+
+    if (selectedShip) {
+      return getDistanceBetweenPoints(selectedShip.position.origin, click) <= panToolRadius();
+    }
+
+    return false;
+  }
+
+  const isClickOnRotate = (click: DOMPointReadOnly): boolean => {
+    const selectedShip = getSelectedShip()!;
+
+    if (selectedShip) {
+      const distanceToCenter = getDistanceBetweenPoints(selectedShip.position.origin, click);
+      
+      return distanceToCenter >= rotateToolInnerRadius()
+          && distanceToCenter <= rotateToolOuterRadius();
+    }
+
+    return false;
+  }
+
+  const handleShipSelection = (click: DOMPointReadOnly) => {
+
+    const clickedShip = findClickedShip(click);
+    setSelectedShipId(clickedShip?.id || null);
+  }
+
+  const getMouseEventPosition = (event: MouseEvent) => {
+    const canvas = document.getElementById('canvas')!;
+    
     const canvasX = event.clientX - canvas.offsetLeft;
     const canvasY = event.clientY - canvas.offsetTop;
-
-    const clickedShipId = findClickedShipId({ x: canvasX, y: canvasY });
-    console.log('ship index', clickedShipId)
     
-    setSelectedShipId(clickedShipId);
+    return new DOMPointReadOnly(canvasX, canvasY);
+  }
+
+  const handleToolsCommand = (clickPosition: DOMPointReadOnly) => {
+    if (isClickOnPan(clickPosition)) {
+
+      setMouseHoldStart(clickPosition);
+      setActiveCommand('pan');
+    } else if (isClickOnRotate(clickPosition)) {
+
+      setMouseHoldStart(clickPosition);
+      setActiveCommand('rotate');
+
+      const selectedShip = getSelectedShip()!;
+      getAngleBetweenPoints(selectedShip.position.origin, clickPosition)
+    }
+  }
+  
+  const handleMouseDown = (event: MouseEvent) => {
+    
+    const clickPosition = getMouseEventPosition(event);
+
+    if (!!selectedShipId() && isClickInTools(getSelectedShip()!, clickPosition)) {
+      handleToolsCommand(clickPosition);
+    } else {
+      handleShipSelection(clickPosition);
+    }
+  }
+
+  const handleMouseUp = (event: MouseEvent) => {
+
+    if (!activeCommand()) {
+      return;
+    }
+
+    if (activeCommand() === 'pan') {
+      handlePanMove(event, true);
+    } else if (activeCommand() === 'rotate') {
+      handleRotate(event, true);
+    }
+  }
+
+  const handleMouseMove = (event: MouseEvent) => {
+
+    if (!activeCommand()) {
+      return;
+    }
+
+    if (activeCommand() === 'pan') {
+      handlePanMove(event, false);
+    } else if (activeCommand() === 'rotate') {
+     handleRotate(event, false);
+    }
+  }
+
+  const handleRotate = (event: MouseEvent, shouldEndRotation: boolean) => {
+
+    const mouseHoldStartPosition = mouseHoldStart();
+    if (!mouseHoldStartPosition) {
+      return;
+    }
+    
+    const selectedShipCenterPoint = getSelectedShip()!.position.origin;
+    const rotateEndPosition = getMouseEventPosition(event);
+    
+    const rotateStartAngle = getAngleBetweenPoints(selectedShipCenterPoint, mouseHoldStartPosition);
+    const rotateEndAngle = getAngleBetweenPoints(selectedShipCenterPoint, rotateEndPosition);
+
+    const modifiedShip = { ...getSelectedShip()! }
+    modifiedShip.position.rotation = modifiedShip.position.rotation + rotateEndAngle - rotateStartAngle;
+    setShips((currentShips) => currentShips
+      .filter(s => s.id !== modifiedShip.id)
+      .concat([modifiedShip])
+    );
+
+    if (shouldEndRotation) {
+      setMouseHoldStart(null);
+      setActiveCommand(null);
+    } else {
+      setMouseHoldStart(rotateEndPosition);
+    }
+  }
+
+  const handlePanMove = (event: MouseEvent, shouldEndPanning: boolean) => {
+
+    const mouseHoldStartPosition = mouseHoldStart();
+    if (!mouseHoldStartPosition) {
+      return;
+    }
+
+    const panEndPosition = getMouseEventPosition(event);
+  
+    const dx = panEndPosition.x - mouseHoldStartPosition.x;
+    const dy = panEndPosition.y - mouseHoldStartPosition.y;
+
+    const modifiedShip = { ...getSelectedShip()! }
+    modifiedShip.position.origin = new DOMPointReadOnly(
+      modifiedShip.position.origin.x + dx, 
+      modifiedShip.position.origin.y + dy 
+    );
+
+    setShips((currentShips) => currentShips
+      .filter(s => s.id !== modifiedShip.id)
+      .concat([modifiedShip])
+    );
+    
+    if (shouldEndPanning) {
+      setMouseHoldStart(null);
+    } else {
+      setMouseHoldStart(panEndPosition);
+    }
   }
 
   return (
@@ -222,6 +410,8 @@ function App() {
         height={CANVAS_WIDTH} 
         style={{ 'border': '1px solid black' }}
         onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
       ></canvas>
     </>
   )
---
+++ src/utils/points.ts
@@ -0,0 +1,22 @@
+export const getDistanceBetweenPoints = (pointA: DOMPointReadOnly, pointB: DOMPointReadOnly) => {
+    return Math.sqrt((pointA.x - pointB.x) ** 2 + (pointA.y - pointB.y) ** 2)
+}
+
+export const getAngleBetweenPoints = (startPoint: DOMPointReadOnly, endPoint: DOMPointReadOnly) => {
+
+    if (startPoint.x === endPoint.x) {
+        return startPoint.y <= endPoint.y ? 0 : 180;
+    }
+
+    if (startPoint.y === endPoint.y) {
+        return startPoint.x < endPoint.x ? 270 : 90;
+    }
+
+    const angleRadians = Math.atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x);
+    const angleDegrees = angleRadians * 180 / Math.PI;
+    
+    const angleCanvasCoordinates = (angleDegrees + 270) % 360;
+    console.log(angleCanvasCoordinates)
+
+    return angleCanvasCoordinates;
+}
